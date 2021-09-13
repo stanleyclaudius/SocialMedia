a96975d5737b70630db1318d073c862ce431f355
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-09-13 14:41:34 +0700

    Create login and register page

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -2,12 +2,16 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 
-const Login = () => {
+const Register = () => {
   const [userData, setUserData] = useState({
+    name: '',
+    username: '',
     email: '',
-    password: ''
+    password: '',
+    confirmPassword: ''
   });
   const [isShowPassword, setIsShowPassword] = useState(false);
+  const [isShowConfirmPassword, setIsShowConfirmPassword] = useState(false);
 
   const handleInputChange = e => {
     const {name, value} = e.target;
@@ -26,6 +30,14 @@ const Login = () => {
         <h1>SR Social</h1>
         <form onSubmit={handleFormSubmit}>
           <div className="inputGroup">
+            <label htmlFor="name">Name</label>
+            <input type="text" id='name' name='name' autoComplete='off' value={userData.name} onChange={handleInputChange} />
+          </div>
+          <div className="inputGroup">
+            <label htmlFor="username">Username</label>
+            <input type="text" id='username' name='username' autoComplete='off' value={userData.username} onChange={handleInputChange} />
+          </div>
+          <div className="inputGroup">
             <label htmlFor="email">Email address</label>
             <input type="text" id='email' name='email' autoComplete='off' value={userData.email} onChange={handleInputChange} />
           </div>
@@ -36,14 +48,21 @@ const Login = () => {
               {isShowPassword ? <AiFillEyeInvisible onClick={() => setIsShowPassword(false)} /> : <AiFillEye onClick={() => setIsShowPassword(true)} />}
             </div>
           </div>
-          <button>Login</button>
+          <div className="inputGroup">
+            <label htmlFor="confirmPassword">Password Confirmation</label>
+            <div className="inputGroup--password">
+              <input type={isShowConfirmPassword ? 'text' : 'password'} id='confirmPassword' name='confirmPassword' autoComplete='off' value={userData.confirmPassword} onChange={handleInputChange} />
+              {isShowConfirmPassword ? <AiFillEyeInvisible onClick={() => setIsShowConfirmPassword(false)} /> : <AiFillEye onClick={() => setIsShowConfirmPassword(true)} />}
+            </div>
+          </div>
+          <button>Register</button>
         </form>
         <p to='/register'>
-          Don't have an account? Click <Link to='/register'>here</Link>
+          Already have an account? Click <Link to='/login'>here</Link>
         </p>
       </div>
     </div>
   )
 }
 
-export default Login;
+export default Register;
