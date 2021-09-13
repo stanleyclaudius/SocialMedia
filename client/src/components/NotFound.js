c4cdd449f54cdcaf8024c1de2de1e2ea407365b3
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-09-13 10:09:06 +0700

    Remove Chakra UI and add SCSS as styling

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,15 +1,8 @@
-import { Center, Heading, Text, VStack } from '@chakra-ui/react';
-
 const NotFound = () => {
   return (
-    <Center h='100vh'>
-      <VStack>
-        <Text fontSize='2xl' fontWeight='bold' color='gray.400' mb='2'>SR Social</Text>
-        <Heading as='h1' size='2xl' color='gray.400'>
-          404 | Not Found
-        </Heading>
-      </VStack>
-    </Center>
+    <div>
+      not found
+    </div>
   )
 }
 
