4e6e0a0a51c15dc7731e45ff827cc480b33de883
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-09-15 22:48:29 +0700

    Create profile header design at profile page

--- client/src/pages/profile/[id].js
+++ client/src/pages/profile/[id].js
@@ -1,8 +1,55 @@
+import { useState } from 'react';
+import Avatar from './../../components/Avatar';
+// import FollowBtn from './../../components/FollowBtn';
+import { BiFemaleSign, BiMaleSign } from 'react-icons/bi';
 
 const Profile = () => {
+  const [isOpenSaved, setIsOpenSaved] = useState(false);
+
   return (
-    <div className='container profile'>
-      profile
+    <div className='container userProfile'>
+      <div className="userProfile__top">
+        <div className="userProfile__left">
+          <Avatar size='big' />
+        </div>
+        <div className="userProfile__right">
+        <div className="userProfile__right--username">
+          <div className="userProfile__right--usernameInfo">
+            <h3>username01</h3>
+            <BiFemaleSign style={{color: 'rgb(255, 156, 172)'}} />
+          </div>
+          {/* <FollowBtn /> */}
+          <button>Edit Profile</button>
+        </div>
+        <div className="userProfile__right--info">
+          <div className='postCount'>
+            101 Post
+          </div>
+          <div className="followerCount">
+            101 Followers
+          </div>
+          <div className="followingCount">
+            101 Followings
+          </div>
+        </div>
+        <p className='fullname'>User01 <span>+61 829283 38934</span></p>
+        <p className='story'>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem, est!
+        </p>
+        <p className='email'>user01@gmail.com</p>
+      </div>
+      </div>
+    
+      <div className="userProfile__bottom">
+        <div className="userProfile__bottom--header">
+          <div onClick={() => setIsOpenSaved(false)} className={`${isOpenSaved ? '' : 'active'}`}>
+            POSTS
+          </div>
+          <div onClick={() => setIsOpenSaved(true)} className={`${isOpenSaved ? 'active' : ''}`}>
+            SAVED
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
