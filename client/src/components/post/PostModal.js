bbdb65ccc33d269f8acc57a87e6799513eac5d8a
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-09-15 20:36:43 +0700

    Create PostModal component to create post

---
+++ client/src/components/post/PostModal.js
@@ -0,0 +1,145 @@
+import { useState, useRef } from 'react';
+import { AiOutlineClose, AiFillCamera, AiFillPicture } from 'react-icons/ai';
+
+const PostModal = ({setIsOpenModal}) => {
+  const [content, setContent] = useState('');
+  const [images, setImages] = useState([]);
+  const [stream, setStream] = useState(false);
+  const [tracks, setTracks] = useState('');
+
+  const videoRef = useRef();
+  const canvasRef = useRef();
+
+  const handleImageChange = e => {
+    const files = [...e.target.files];
+    const newImages = [];
+
+    files.forEach(file => {
+      newImages.push(file);
+    });
+
+    setImages([...images, ...newImages]);
+  }
+
+  const deleteImage = index => {
+    const imgCopy = [...images];
+    imgCopy.splice(index, 1);
+    setImages(imgCopy);
+  }
+
+  const handleStartStream = () => {
+    setStream(true);
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({video: true})
+        .then(mediaStream => {
+          videoRef.current.srcObject = mediaStream;
+          videoRef.current.play();
+
+          const track = mediaStream.getTracks();
+          setTracks(track[0]);
+        })
+        .catch(err => console.log(err));
+    }
+  }
+
+  const handleStopStream = () => {
+    setStream(false);
+    tracks.stop();
+  }
+
+  const handleCapture = () => {
+    const width = videoRef.current.clientWidth;
+    const height = videoRef.current.clientHeight;
+
+    canvasRef.current.setAttribute('width', width);
+    canvasRef.current.setAttribute('height', height);
+
+    const ctx = canvasRef.current.getContext('2d');
+    ctx.drawImage(videoRef.current, 0, 0, width, height);
+
+    let cameraImageURL = canvasRef.current.toDataURL();
+    setImages([...images, {camera: cameraImageURL}]);
+
+    setStream(false);
+    tracks.stop();
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  }
+
+  return (
+    <div className='postModal'>
+      <div className="postModal__box">
+        <div className="postModal__box--header">
+          <h3>Create Post</h3>
+          <AiOutlineClose onClick={() => setIsOpenModal(false)} />
+        </div>
+        <div className="postModal__box--content">
+          <form onSubmit={handleSubmit}>
+            <div>
+              <textarea placeholder='Your caption here ...' value={content} onChange={e => setContent(e.target.value)} />
+              <div className="postImage">
+                <h4>Image Upload</h4>
+                <div className="postImage__icon">
+                  {
+                    stream
+                    ? <AiFillCamera onClick={handleCapture} />
+                    : (
+                      <>
+                        <AiFillCamera onClick={handleStartStream} />
+                        <div className='fileInput'>
+                          <input type="file" multiple accept='image/*,video/*' onChange={handleImageChange} />
+                          <AiFillPicture />
+                        </div>
+                      </>
+                    )
+                  }
+                </div>
+                
+                {
+                  stream
+                  ? (
+                    <div className='postImage__video'>
+                      <video ref={videoRef} autoPlay muted width="100%" />
+                      <AiOutlineClose onClick={handleStopStream} />
+                      <canvas ref={canvasRef} style={{display: 'none'}} />
+                    </div>
+                  )
+                  : (
+                    <div className="postImage__container">
+                      {
+                        images.map((img, i) => (
+                          <div key={i}>
+                            {
+                              img.camera
+                              ? <img src={img.camera} alt='Post' />
+                              : img.type.match(/video/i)
+                                ? <video src={URL.createObjectURL(img)} controls />
+                                : (
+                                  <img src={
+                                    img.camera
+                                    ? img.camera
+                                    : URL.createObjectURL(img)
+                                  } alt='Post' />
+                                )
+                            }
+                            <AiOutlineClose onClick={() => deleteImage(i)} />
+                          </div>
+                        ))
+                      }
+                    </div>
+                  )
+                }
+              </div>
+            </div>
+            <button type='submit'>Submit</button>
+            <div className="clear"></div>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PostModal;
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,18 +1,28 @@
+import { useState } from 'react';
 import PostCard from './../components/post/PostCard';
 import Avatar from './../components/Avatar';
 import UserCard from '../components/UserCard';
 import { MdRefresh } from 'react-icons/md';
+import PostModal from './../components/post/PostModal';
 
 const Home = () => {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
   return (
+    <>
     <div className='container home'>
       <div className="homeLeft">
         <PostCard />
       </div>
       <div className="homeRight">
         <div className="homeRight__user">
-          <Avatar size='medium' />
-          <p>username01</p>
+          <div>
+            <Avatar size='medium' />
+            <p>username01</p>
+          </div>
+          <div>
+            <button onClick={() => setIsOpenModal(true)}>Create Post</button>
+          </div>
         </div>
         <div className='reloadGroup'>
           <h4>Suggestions For You</h4>
@@ -25,6 +35,9 @@ const Home = () => {
         </div>
       </div>
     </div>
+
+    {isOpenModal && <PostModal setIsOpenModal={setIsOpenModal} />}
+    </>
   )
 }
 
