0c01a2052e5b3e4d13d222337787e4f706e0acec
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-09-16 10:22:15 +0700

    Add discover page

---
+++ client/src/pages/discover.js
@@ -0,0 +1,21 @@
+import PostThumbnail from './../components/PostThumbnail';
+
+const discover = () => {
+  return (
+    <div className='container discover'>
+      <PostThumbnail />
+      <PostThumbnail />
+      <PostThumbnail />
+      <PostThumbnail />
+      <PostThumbnail />
+      <PostThumbnail />
+      <PostThumbnail />
+      <PostThumbnail />
+      <PostThumbnail />
+      <PostThumbnail />
+      <PostThumbnail />
+    </div>
+  )
+}
+
+export default discover;
