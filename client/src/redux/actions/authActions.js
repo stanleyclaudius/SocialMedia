367847c56633a1f2f00b693daa371b95663d99e0
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-09-18 21:05:39 +0700

    Add localStorage to optimize refreshToken function

--- client/src/redux/actions/authActions.js
+++ client/src/redux/actions/authActions.js
@@ -11,6 +11,7 @@ export const register = (userData) => async(dispatch) => {
     });
 
     const res = await postDataAPI('register', userData);
+    localStorage.setItem('logged', true);
     dispatch({
       type: GLOBALTYPES.AUTH,
       payload: {
@@ -45,6 +46,7 @@ export const login = (userData) => async(dispatch) => {
     });
 
     const res = await postDataAPI('login', userData);
+    localStorage.setItem('logged', true);
     dispatch({
       type: GLOBALTYPES.AUTH,
       payload: {
@@ -70,39 +72,43 @@ export const login = (userData) => async(dispatch) => {
 }
 
 export const refreshToken = () => async(dispatch) => {
-  try {
-    dispatch({
-      type: GLOBALTYPES.ALERT,
-      payload: {
-        loading: true
-      }
-    });
+  const logged = localStorage.getItem('logged');
+  if (logged) {
+    try {
+      dispatch({
+        type: GLOBALTYPES.ALERT,
+        payload: {
+          loading: true
+        }
+      });
 
-    const res = await postDataAPI('refresh_token');
-    dispatch({
-      type: GLOBALTYPES.AUTH,
-      payload: {
-        token: res.data.accessToken,
-        user: res.data.user
-      }
-    });
+      const res = await postDataAPI('refresh_token');
+      dispatch({
+        type: GLOBALTYPES.AUTH,
+        payload: {
+          token: res.data.accessToken,
+          user: res.data.user
+        }
+      });
 
-    dispatch({
-      type: GLOBALTYPES.ALERT,
-      payload: {}
-    });
-  } catch (err) {
-    dispatch({
-      type: GLOBALTYPES.ALERT,
-      payload: {
-        error: err.response.data.msg
-      }
-    });
+      dispatch({
+        type: GLOBALTYPES.ALERT,
+        payload: {}
+      });
+    } catch (err) {
+      dispatch({
+        type: GLOBALTYPES.ALERT,
+        payload: {
+          error: err.response.data.msg
+        }
+      });
+    }
   }
 }
 
 export const logout = () => async(dispatch) => {
   try {
+    localStorage.removeItem('logged');
     await postDataAPI('logout');
     window.location.href='/';
   } catch (err) {
