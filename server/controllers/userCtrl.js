b29bf1cd86d8b98f2db956c55cdeb2ccf28f7f22
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-09-18 23:51:59 +0700

    Add search user function at backend

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -1,6 +1,15 @@
 const User = require('./../models/User');
 
 const userCtrl = {
+  searchUser: async(req, res) => {
+    const {username} = req.query;
+    try {
+      const user = await User.find({username: {$regex: username}}).limit(10).select('username fullname avatar');
+      res.status(200).json({user});
+    } catch (err) {
+      return res.status(500).json({msg: err.message});
+    }
+  },
   getProfile: async(req, res) => {
     try {
       const {id} = req.params;
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const userCtrl = require('./../controllers/userCtrl');
 
+router.route('/search').get(userCtrl.searchUser);
 router.route('/profile/:id').get(userCtrl.getProfile);
 
 module.exports = router;
