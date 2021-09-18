acfc0638bd9a9b6e3727301c8fe143926c4858cb
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-09-18 11:28:27 +0700

    Setup redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import DataProvider from './redux/store';
 import './styles/css/main.css';
 
 ReactDOM.render(
-  <App />,
+  <DataProvider>
+    <App />
+  </DataProvider>,
   document.getElementById('root')
 );
---
+++ client/src/redux/store.js
@@ -0,0 +1,20 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+const DataProvider = ({children}) => {
+  return (
+    <Provider store={store}>
+      {children}
+    </Provider>
+  );
+}
+
+export default DataProvider;
