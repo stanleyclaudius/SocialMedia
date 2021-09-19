29484a95a8e5255544640435984c7fd80d2cb26f
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-09-19 10:28:12 +0700

    Add middleware at backend

---
+++ server/middlewares/auth.js
@@ -0,0 +1,25 @@
+const User = require('./../models/User');
+const jwt = require('jsonwebtoken');
+
+const isAuthenticated = async(req, res, next) => {
+  try {
+    const token = req.header('Authorization');
+    if (!token)
+      return res.status(400).json({msg: 'Invalid authentication.'});
+
+    const decoded = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
+    if (!decoded)
+      return res.status(400).json({msg: 'Invalid authentication.'});
+
+    const user = await User.findById(decoded.id);
+    if (!user)
+      return res.status(404).json({msg: 'User not found.'});
+
+    req.user = user;
+    next();
+  } catch (err) {
+    return res.status(500).json({msg: err.message});
+  }
+}
+
+module.exports = isAuthenticated;
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const userCtrl = require('./../controllers/userCtrl');
+const isAuthenticated = require('./../middlewares/auth');
 
-router.route('/search').get(userCtrl.searchUser);
-router.route('/profile/:id').get(userCtrl.getProfile);
+router.route('/search').get(isAuthenticated, userCtrl.searchUser);
+router.route('/profile/:id').get(isAuthenticated, userCtrl.getProfile);
 
 module.exports = router;
