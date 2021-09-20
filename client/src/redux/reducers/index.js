24d44bf2a7b3eabb76ea428f55df3d367c80cc1b
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-09-20 23:12:48 +0700

    Change post reducer name become homePost

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -2,11 +2,11 @@ import { combineReducers } from 'redux';
 import alert from './alertReducer';
 import auth from './authReducer';
 import profile from './profileReducer';
-import post from './postReducer';
+import homePost from './postReducer';
 
 export default combineReducers({
   alert,
   auth,
   profile,
-  post
+  homePost
 });
