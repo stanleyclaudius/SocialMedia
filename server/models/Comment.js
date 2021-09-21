da01d1815624ee1cf3d6b9f74e7826d606301074
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-09-21 10:14:49 +0700

    Create comment model

---
+++ server/models/Comment.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true
+  },
+  tag: Object,
+  reply: {
+    type: mongoose.Types.ObjectId,
+    ref: 'comment'
+  },
+  likes: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: 'user'
+    }
+  ],
+  user: {
+    type: mongoose.Types.ObjectId,
+    ref: 'user'
+  },
+  postId: {
+    type: mongoose.Types.ObjectId,
+    ref: 'post'
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('comment', commentSchema);
