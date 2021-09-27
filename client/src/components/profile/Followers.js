8929bb72327e1bb0be2498dcd4590525a75d73f6
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-09-27 19:47:37 +0700

    Add not found to follower and following modal

--- client/src/components/profile/Followers.js
+++ client/src/components/profile/Followers.js
@@ -7,14 +7,26 @@ const Followers = ({followers, active, setIsOpenFollowers}) => {
     <div className={`followModal ${active ? 'active' : ''}`}>
       <div className={`followModal__box ${active ? 'active' : ''}`}>
         <div className="followModal__header">
-          <h3>Followers</h3>
+          <h3>{followers?.length > 1 ? 'Followers' : 'Follower'}</h3>
           <AiOutlineClose onClick={() => setIsOpenFollowers(false)} />
         </div>
         <div className="followModal__body">
           {
-            followers?.map(item => (
-              <UserCard key={item._id} user={item} setIsOpenFollowers={setIsOpenFollowers} />
-            ))
+            followers?.length === 0
+            ? (
+              <div style={{color: 'red', textAlign: 'center'}}>
+                <h3>No Follower Found.</h3>
+              </div>
+            )
+            : (
+              <>
+                {
+                  followers?.map(item => (
+                    <UserCard key={item._id} user={item} setIsOpenFollowers={setIsOpenFollowers} />
+                  ))
+                }
+              </>
+            )
           }
         </div>
       </div>
--- client/src/components/profile/Followings.js
+++ client/src/components/profile/Followings.js
@@ -6,14 +6,26 @@ const Followers = ({followings, active, setIsOpenFollowings}) => {
     <div className={`followModal ${active ? 'active' : ''}`}>
       <div className={`followModal__box ${active ? 'active' : ''}`}>
         <div className="followModal__header">
-          <h3>Followings</h3>
+          <h3>{followings?.length > 1 ? 'Followings' : 'Following'}</h3>
           <AiOutlineClose onClick={() => setIsOpenFollowings(false)} />
         </div>
         <div className="followModal__body">
           {
-            followings?.map(item => (
-              <UserCard key={item._id} user={item} setIsOpenFollowings={setIsOpenFollowings} />
-            ))
+            followings?.length === 0
+            ? (
+              <div style={{color: 'red', textAlign: 'center'}}>
+                <h3>No Following Found.</h3>
+              </div>
+            )
+            : (
+              <>
+                {
+                  followings?.map(item => (
+                    <UserCard key={item._id} user={item} setIsOpenFollowings={setIsOpenFollowings} />
+                  ))
+                }
+              </>
+            )
           }
         </div>
       </div>
