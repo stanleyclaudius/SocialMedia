55aad1564ba6103fe412f783394ea0ae917727fc
Stanley Claudius <stanleyclaudiuss@gmail.com>
2021-10-02 16:24:06 +0700

    Create show and hide comments at a post

--- client/src/components/comment/CommentContainer.js
+++ client/src/components/comment/CommentContainer.js
@@ -2,25 +2,46 @@ import { useState, useEffect } from 'react';
 import CommentDisplay from './CommentDisplay';
 
 const CommentContainer = ({post}) => {
-  const [filteredComments, setFilteredComments] = useState([]);
+  const [comments, setComments] = useState([]);
+  const [showComments, setShowComments] = useState([]);
+  // const [filteredComments, setFilteredComments] = useState([]);
   const [replyComment, setReplyComment] = useState([]);
+  const [next, setNext] = useState(2);
 
-  useEffect(() => {
-    const newComments = post.comments.filter(comm => !comm.reply);
-    setFilteredComments(newComments);
-  }, [post.comments]);
+  // useEffect(() => {
+  //   const newComments = post.comments.filter(comm => !comm.reply);
+  //   setFilteredComments(newComments);
+  // }, [post.comments]);
 
   useEffect(() => {
     const newComments = post.comments.filter(comm => comm.reply);
     setReplyComment(newComments);
   }, [post.comments]);
 
+  useEffect(() => {
+    const newComments = post.comments.filter(comm => !comm.reply);
+    setComments(newComments);
+    setShowComments(newComments.slice(newComments.length - next));
+  }, [post.comments, next]);
+
   return (
     <>
       {
-        filteredComments.map(comm => (
+        showComments.map(comm => (
           <CommentDisplay key={comm._id} comment={comm} post={post} replyComment={replyComment.filter(item => item.reply === comm._id)} />
         ))
+      }
+      
+      {
+        comments.length - next > 0
+        ? <div style={{cursor: 'pointer', color: 'blue', fontSize: '13px', width: 'fit-content'}} onClick={() => setNext(comments.length)}>
+          See more comments ...
+        </div>
+
+        : comments.length > 2 &&
+        <div style={{cursor: 'pointer', color: 'blue', fontSize: '13px', width: 'fit-content'}} onClick={() => setNext(2)}>
+          Hide comments ...
+        </div>
       }
     </>
   )
